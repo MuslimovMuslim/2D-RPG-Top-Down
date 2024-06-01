945e80b23d8e343d6207d323032e475c9482760f
MuslimovMuslim <132162229+MuslimovMuslim@users.noreply.github.com>
2024-06-01 10:25:45 +0500

    0

--- Assets/Scripts/Player/ActiveWeapon.cs
+++ Assets/Scripts/Player/ActiveWeapon.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Этот класс ActiveWeapon отвечает за управление текущим активным оружием игрока в игре.
 public class ActiveWeapon : Singleton<ActiveWeapon>
 {
     public MonoBehaviour CurrentActiveWeapon { get; private set; }
@@ -17,11 +18,11 @@ public class ActiveWeapon : Singleton<ActiveWeapon>
         playerControls = new PlayerControls();
     }
 
+//Методы OnEnable() и Start(): Эти методы используются для активации PlayerControls и установки обработчиков событий для атаки
     private void OnEnable()
     {
         playerControls.Enable();
     }
-
     private void Start()
     {
         playerControls.Combat.Attack.started += _ => StartAttacking();
@@ -34,6 +35,7 @@ public class ActiveWeapon : Singleton<ActiveWeapon>
         Attack();
     }
 
+//Этот метод устанавливает новое оружие как текущее активное оружие игрока.
     public void NewWeapon(MonoBehaviour newWeapon) {
         CurrentActiveWeapon = newWeapon;
 
@@ -41,9 +43,12 @@ public class ActiveWeapon : Singleton<ActiveWeapon>
         timeBetweenAttacks = (CurrentActiveWeapon as IWeapon).GetWeaponInfo().weaponCooldown;
     }
 
+//Этот метод устанавливает текущее активное оружие в значение null, то есть игрок больше не держит оружие.
     public void WeaponNull() {
         CurrentActiveWeapon = null;
     }
+
+//Этот метод запускает отсчет времени между атаками перед следующей атакой.
 
     private void AttackCooldown() {
         isAttacking = true;
@@ -51,21 +56,23 @@ public class ActiveWeapon : Singleton<ActiveWeapon>
         StartCoroutine(TimeBetweenAttacksRoutine());
     }
 
+//Этот метод реализует отсчет времени между атаками.
     private IEnumerator TimeBetweenAttacksRoutine() {
         yield return new WaitForSeconds(timeBetweenAttacks);
         isAttacking = false;
     }
 
+//StartAttacking() и StopAttacking(): Эти методы обрабатывают нажатие и отпускание кнопки атаки.
     private void StartAttacking()
     {
         attackButtonDown = true;
     }
-
     private void StopAttacking()
     {
         attackButtonDown = false;
     }
 
+//Этот метод вызывает атаку, если игрок нажимает кнопку атаки, и проверяет, доступно ли оружие и не идет ли уже атака.
     private void Attack() {
         if (attackButtonDown && !isAttacking && CurrentActiveWeapon) {
             AttackCooldown();
